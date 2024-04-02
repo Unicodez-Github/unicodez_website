9d75a2742cf343e41bdc8e9f10cec860a95a3529
SauravYadav12 <saurav1286@gmail.com>
2024-04-03 02:01:41 +0530

    changes in footer address

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -163,10 +163,17 @@ export default function ContactForm() {
       <div className="container">
         <div className="grid lg:grid-cols-2 gap-12">
           <div className="bg-[#FFF2CD] p-10 rounded-3xl">
-
             <h2 className="section-title font-medium text-lg lg:text-3xl lg:leading-snug max-w-md b">
               Leave us a little info, and we&lsquo;ll be in touch.
-              {modal && <VerificationModal inProgress={modalProgress} setotps={setotps} open={modal} setModal={setModal} submitUserOTP={submitUserOTP} />}
+              {modal && (
+                <VerificationModal
+                  inProgress={modalProgress}
+                  setotps={setotps}
+                  open={modal}
+                  setModal={setModal}
+                  submitUserOTP={submitUserOTP}
+                />
+              )}
             </h2>
             <form>
               <div className="space-y-10 mt-16">
@@ -176,10 +183,9 @@ export default function ContactForm() {
                   placeholder="Enter Your Name"
                   name="from_name"
                   required
-                  value={form.from_name || ''}
+                  value={form.from_name || ""}
                   onChange={formHandler}
                 />
-
 
                 <PhoneInput
                   containerClass="st"
@@ -187,9 +193,8 @@ export default function ContactForm() {
                   className="w-full border appearance-none rounded bg-white text-unicodez-dark text-base py-3 px-6"
                   name="from_name"
                   country={"us"}
-                  onChange={setphoneNumber} />
-
-
+                  onChange={setphoneNumber}
+                />
 
                 <input
                   type="email"
@@ -198,7 +203,6 @@ export default function ContactForm() {
                   required
                   name="from_email"
                   onChange={formHandler}
-
                 />
                 <textarea
                   rows={10}
@@ -210,20 +214,27 @@ export default function ContactForm() {
                 />
 
                 <div className="mb-10">
-                  {(inProgress) ? <Circles
-                    height="40"
-                    width="40"
-                    color="#3498db"
-                    ariaLabel="circles-loading"
-                    wrapperStyle={{}}
-                    wrapperClass=""
-                    visible={true}
-                  /> :
-                    <button id="verifyForm" className="button primary normal disabled:bg-slate-100" onClick={submitHandler} >Submit</button>
-                  }
+                  {inProgress ? (
+                    <Circles
+                      height="40"
+                      width="40"
+                      color="#3498db"
+                      ariaLabel="circles-loading"
+                      wrapperStyle={{}}
+                      wrapperClass=""
+                      visible={true}
+                    />
+                  ) : (
+                    <button
+                      id="verifyForm"
+                      className="button primary normal disabled:bg-slate-100"
+                      onClick={submitHandler}
+                    >
+                      Submit
+                    </button>
+                  )}
 
                   <div id="verify"></div>
-
                 </div>
               </div>
             </form>
@@ -241,15 +252,18 @@ export default function ContactForm() {
                 <span>(9 am to 7 pm EST)</span>
               </div>
             </div>
-           <div class="mapouter">
-                    <div class="gmap_canvas">
-                    <iframe width="560" height="360" id="gmap_canvas" src="https://maps.google.com/maps?q=1000+North+West%2C%231281-252%2C+wilmington%2C+DE+-+19801&t=&z=10&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
-                      </iframe><a href="https://online.stopwatch-timer.net">
-                      </a><br><a href="https://textcaseconvert.com"></a><br>
-                      <style>.mapouter{position: relative;text-align: right;height: 360px;width: 560px;}</style><a href="https://www.ongooglemaps.com">google maps embed</a><style>.gmap_canvas{overflow: hidden;background: none !important;height: 360px;width: 560px;}
-                      </style>
-                      </div>
-                    </div>
+            <div className="mt-10">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3067.7557670560195!2d-75.5533080843079!3d39.74513670413234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6fd6a9dfc32a9%3A0xab073c5880e220d7!2s831%20N%20Tatnall%20St%2C%20Wilmington%2C%20DE%2019801%2C%20USA!5e0!3m2!1sen!2sng!4v1660511553349!5m2!1sen!2sng"
+                width="100%"
+                height="360"
+                style={{ border: 0 }}
+                allowFullScreen={false}
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                className="rounded-[20px]"
+              />
+            </div>
             <div className="grid grid-cols-2 mt-10">
               <div>
                 <h4 className="text-unicodez-dark font-bold text-lg uppercase">

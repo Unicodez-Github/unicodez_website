522fbe615886fbf6c70b89cbb1beb26f2be4a05f
Unicodez <86790978+Unicodez-Github@users.noreply.github.com>
2024-04-03 01:35:38 +0530

    Update footer.js

--- components/sections/footer.js
+++ components/sections/footer.js
@@ -53,8 +53,7 @@ const Footer = () => {
                     height={28}
                     width={28}
                   />{" "}
-                  <span>1000 North West Street, #1281-252</span>
-                  <span>Wilmington , DE – 19801 USA</span>
+                  <span>1000 North West Street, #1281-252, Wilmington , DE – 19801 USA</span>
                 </li>
                 <li className="flex items-start space-x-2.5">
                   <Image

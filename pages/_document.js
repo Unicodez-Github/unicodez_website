1221207e33af3f077ff3d4255e3b72bf43c82f93
Unicodez <86790978+Unicodez-Github@users.noreply.github.com>
2025-03-19 01:19:47 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -9,46 +9,73 @@ export default function Document() {
           name="google-site-verification"
           content="7LYu2L_uKQUk9L8Vyd6KDCDZzxLF7KsOd5Lfk1jHclY"
         />
+        
         <Script
           id="google-analytics"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-N7XS96X');
-          `,
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),
+                    dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N7XS96X');
+            `,
           }}
         />
-        <Script>
-          _linkedin_partner_id = "8188465";
-          window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-          window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-        </Script>
-        <Script>
-          (function(l) {
-          if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
-          window.lintrk.q=[]}
-          var s = document.getElementsByTagName("script")[0];
-          var b = document.createElement("script");
-          b.type = "text/javascript";b.async = true;
-          b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-          s.parentNode.insertBefore(b, s);})(window.lintrk);
-        </Script>
+        
+        <Script
+          id="linkedin-partner"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              _linkedin_partner_id = "8188465";
+              window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+              window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+            `,
+          }}
+        />
+        <Script
+          id="linkedin-insight"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(l) {
+                if (!l) {
+                  window.lintrk = function(a,b) {
+                    window.lintrk.q.push([a,b]);
+                  };
+                  window.lintrk.q = [];
+                }
+                var s = document.getElementsByTagName("script")[0];
+                var b = document.createElement("script");
+                b.type = "text/javascript";
+                b.async = true;
+                b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                s.parentNode.insertBefore(b, s);
+              })(window.lintrk);
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
+
         <noscript>
-          <img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=8188465&fmt=gif" />
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=8188465&fmt=gif"
+          />
         </noscript>
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N7XS96X" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-          }}
-        ></noscript>
       </body>
     </Html>
   );
